fef6d4ad6e24bda217a870df342f8348ba17b2ec
fsanzl <>
2026-09-10 10:05:33 +0200

    path

--- fetch_data_app.sh
+++ fetch_data_app.sh
@@ -8,7 +8,7 @@ echo ${BRANCH}
 rm -rf kalbeck-tagebuch-static* html xslt *.zip build.xml saxon *scripts *lock
 curl -L -o "${BRANCH}.zip" "https://github.com/max-kalbeck/kalbeck-tagebuch-static/archive/${BRANCH}.zip"
 unzip ${BRANCH}.zip
-rm -f kalbeck-tagebuch-static-${BRANCH}/pyproject.toml kalbeck-tagebuch-static-${BRANCH}/README*   kalbeck-tagebuch-static-${BRANCH}/set* 
+rm -f kalbeck-tagebuch-static-${BRANCH}/pyproject.toml kalbeck-tagebuch-static-${BRANCH}/README*  kalbeck-tagebuch-static-${BRANCH}/set* 
 mv kalbeck-tagebuch-static-${BRANCH}/*.* ./
 mv kalbeck-tagebuch-static-${BRANCH}/{saxon,xslt,html,*scripts} .
 echo 'rm -rf data/editions' >> shellscripts/fetch_data.sh
